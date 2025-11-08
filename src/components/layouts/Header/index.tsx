f7c2695ee25db3679cdfbb88f354da8dbfebd007
Aydan SOUPAMA <aydansoupama@gmail.com>
2025-11-08 19:22:23 +0100

    fix: set up a variable size for the header

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -4,11 +4,9 @@ import ChooseGame, { ChooseGameDropdown, ChooseGameHeaderDropdown } from "./choo
 import Logo from "../Logo";
 import LanguageEditor from "./language-editor";
 import Auth from "./auth";
-import { ChevronDown, Menu, X } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import Image from "next/image";
-import { games } from "@/datas/games";
 
 const Header = ({ user }: { user: User | undefined }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -16,10 +14,8 @@ const Header = ({ user }: { user: User | undefined }) => {
     null
   );
 
-  const [isGameDropdownOpen, setIsGameDropdownOpen] = useState(false);
-
   return (
-    <header className="w-full h-20 flex items-center justify-between py-2 px-4 md:px-[2vw] relative">
+    <header className="w-full h-16 md:h-[10vh] flex items-center justify-between py-2 px-4 md:px-[2vw] relative">
       <div className="flex justify-center items-center gap-4 sm:gap-6 lg:gap-8">
         {/* Menu hamburger - mobile only */}
         <button

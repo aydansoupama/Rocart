bc0e6930c48ce667c372782c817f96a9f9202652
Aydan SOUPAMA <aydansoupama@gmail.com>
2025-11-10 23:08:11 +0100

    fix: typing for hexToRgba

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function hexToRgba(hex, alpha) {
+export function hexToRgba(hex: string, alpha: number): string {
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
   const b = parseInt(hex.slice(5, 7), 16);

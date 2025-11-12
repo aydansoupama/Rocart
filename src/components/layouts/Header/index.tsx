d87cb1721a9c715398f323efa6a037a0c007f8ce
Aydan SOUPAMA <aydansoupama@gmail.com>
2025-11-12 19:18:02 +0100

    fix: header main logo

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -26,7 +26,7 @@ const Header = ({ user }: { user: User | undefined }) => {
         </button>
 
         <div>
-          <Logo size={10} className="w-auto h-[5vh]" />
+          <Logo size={10} className="w-auto h-[4.25vh]" />
         </div>
 
         {/* Choose Game - desktop only */}

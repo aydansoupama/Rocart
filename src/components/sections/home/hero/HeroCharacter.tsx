636e92111c2b5ddc52a454139fa37c3c3cebdb68
Aydan SOUPAMA <aydansoupama@gmail.com>
2025-11-12 19:32:25 +0100

    fix: hero character floating items height

--- src/components/sections/home/hero/HeroCharacter.tsx
+++ src/components/sections/home/hero/HeroCharacter.tsx
@@ -6,7 +6,7 @@ export const HeroCharacter = () => {
   return (
     <div className="hidden lg:flex flex-1 relative aspect-[4/3] w-full max-w-[40vw] mx-auto">
       <img
-        className="absolute top-[25vh] left-[30%] w-[50%] h-auto object-contain"
+        className="absolute top-[25vh] left-[30%] object-contain"
         alt="Character"
         src="/logo/char.png"
       />
